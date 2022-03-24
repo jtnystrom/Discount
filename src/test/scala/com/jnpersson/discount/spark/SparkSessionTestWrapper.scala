18d3f8f804c5abbba79a21d756f806104e5d4b14
Johan Nystrom <johan@monomorphic.org>
2022-03-24 16:28:03 +0900

    Restructure SparkSessionTestWrapper to avoid having a spark package object in test/ (already existed in src/ and this confuses the compiler)

---
+++ src/test/scala/com/jnpersson/discount/spark/SparkSessionTestWrapper.scala
@@ -0,0 +1,19 @@
+package com.jnpersson.discount.spark
+
+import org.apache.spark.sql.SparkSession
+
+trait SparkSessionTestWrapper {
+  /*
+    SparkSession for unit tests.
+    We use a very small max split size, to ensure that the relatively small test files
+    end up generating multiple splits, which leads to more code paths being tested.
+   */
+  lazy val spark: SparkSession = {
+    SparkSession
+      .builder()
+      .config("mapreduce.input.fileinputformat.split.maxsize", s"${64 * 1024}")
+      .master("local")
+      .appName("spark test example")
+      .getOrCreate()
+  }
+}
